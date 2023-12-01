a5a754f0f90953f11fd85d315101ab3224f849f4
DominicBarnett <dominic.barnett@icloud.com>
2023-11-30 16:57:01 -0800

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -18,17 +18,32 @@ class Bomb(GameObject):
             self.reset()
         # Update the rect attribute
         self.rect.topleft = (self.x, self.y)
+        
 
     def reset(self):
         # Randomly choose whether the bomb comes down or scrolls from the left
         choice_direction = choice(["down", "left"])
         if choice_direction == "down":
-            self.dx = 0
-            self.dy = (randint(0, 200) / 100) + 1
             self.x = choice(lanes)
             self.y = -64
+            self.dx = 0
+            self.dy = (randint(0, 200) / 100) + 1
+            
         elif choice_direction == "left":
-            self.dx = (randint(0, 200) / 100) + 1
-            self.dy = 0
             self.x = -64
             self.y = choice(lanes)
+            self.dx = (randint(0, 200) / 100) + 1
+            self.dy = 0
+            
+
+    # def reset(self):
+    # direction = choice(u_or_d)
+    # if direction == 'up':
+    #   self.x = choice(lanes_x)
+    #   self.y = 670
+    #   self.dy = -speed
+    # else:
+    #   self.x = choice(lanes_x)
+    #   self.y = -70
+    #   self.dy = speed
+        
